7f23f9979602be195153878de0ca96ce18ae8554
Esther <esther.ruama@hotmail.com>
2024-04-13 11:42:28 -0300

    atual

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('dicas_treino', views.dicas_treino, name='dicas_treino'),
     path('cadastro_treino', views.cadastro_treino, name='cadastro_treino'),
     path('editar_treino/<int:id_treino', views.editar_treino, name='editar_treino'),
+    path('', views.index, name='index_usuario'),
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'usuario',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
